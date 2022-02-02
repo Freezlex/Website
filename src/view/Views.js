4e683fd06c5ad670a35c000536ce3b3765ebbae5
Freezlex <sonyc098@gmail.com>
2022-02-02 10:55:25 +0100

    ref:Angular

--- src/view/Views.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from "./Home";
-export { default as Twitch } from "./Twitch";
-export { default as Discord } from "./Discord";
-export { default as PageNotFound} from './PageNotFound'
