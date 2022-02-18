f39b9c85eb7cce3a7b0fd4c3553dc2f176b6108e
Freezlex <sonyc098@gmail.com>
2022-02-18 17:27:57 +0100

    feat: Adding home page, create app router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component'
+import {ProjectsComponent } from './applications/projects/projects.component'
+import { BlogComponent } from './applications/blog/blog.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomepageComponent},
+  {path: "app", children: [
+      {path: "projects", component: ProjectsComponent, outlet: "application"},
+      {path: "blog", component: BlogComponent, outlet: "application"},
+    ]},
+  {path: '**', pathMatch: 'full', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProjectsComponent } from './applications/projects/projects.component';
+import { ApplicationsComponent } from './applications/applications.component';
+import { HeaderComponent } from './applications/header/header.component';
+import { FooterComponent } from './applications/footer/footer.component';
+import { BlogComponent } from './applications/blog/blog.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ProjectsComponent,
+    ApplicationsComponent,
+    HeaderComponent,
+    FooterComponent,
+    BlogComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
