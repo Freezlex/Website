c1cf89bf692fad35b5e87ebb7ece89bb93aef5d0
Freezlex <sonyc098@gmail.com>
2023-10-11 23:30:20 +0200

    wip: Changing background to some fancy gradient and just define some properties first

--- src/app/components/home/homeCard/home-cards.component.tsx
+++ src/app/components/home/homeCard/home-cards.component.tsx
@@ -6,7 +6,7 @@ export default function HomeCards() {
     return<div id='grid-more' className='card-grid'>
         <div className='card-item accent1' id='one'>p</div>
         <div className='card-item secondary' id='two'>p</div>
-        <div className='card-item primary hover' id='three'>
+        <div className='card-item primary' id='three'>
             <h2><Link to='/projects'>My projects</Link></h2>
             <p>All my current and upcoming projects</p>
         </div>
